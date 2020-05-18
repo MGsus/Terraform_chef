c42ab10c41855382a2962a79aaca1fb9ffdf5f0e
Jesús Orlando Montoya Mejía <jesus.orlando.montoya.mejia@ibm.com>
2020-05-18 15:01:48 -0500

    Chef commands

--- main.tf
+++ main.tf
@@ -11,4 +11,14 @@ resource "ibm_compute_vm_instance" "chef-server" {
   memory               = 1024
   disks                = [25]
   local_disk           = false
+
+  provisioner "remote-exec" {
+    inline = [
+      "wget -O /tmp/chef.rpm https://MYSERVER/chef_installers/chef-15.8.23-1.el7.x86_64.rpm",
+      "rpm -Uvh /tmp/chef.rpm",
+      "wget -O /tmp/base.tgz https://MYSERVER/policyfiles/base.tgz",
+      "tar -C /tmp -xzf /tmp/base.tgz",
+      "PWD=/tmp/base chef-client -z",
+    ]
+  }
 }
